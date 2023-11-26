d7f7f86fbb5fa9b70a7f26d4c4cfdbf40621ad6d
raulgil2807 <115884465+raulgil2807@users.noreply.github.com>
2023-11-26 19:53:05 +0100

    Add files via upload

---
+++ Videojuego/rpg-main/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"rpg","l":"Armas"},{"p":"rpg","l":"Enemigo"},{"p":"rpg","l":"Jugador"},{"p":"rpg","l":"main"}];updateSearchResults();
